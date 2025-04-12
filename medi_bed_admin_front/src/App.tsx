ed02c91ff2cc50d37034032b546bdcb0f5748523
shyamilitc25 <shyamilitc@gmail.com>
2025-04-12 16:50:04 +0530

    added toaster

--- medi_bed_admin_front/src/App.tsx
+++ medi_bed_admin_front/src/App.tsx
@@ -1,7 +1,13 @@
 import AppRoutes from "./routes/routes"
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function App() {
   return (
     <>
+    <ToastContainer />
+
     <AppRoutes/>
     </>
   )
